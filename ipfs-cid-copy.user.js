0551a110e6b7f116616961efe54dca43caef9271
CL <167851968+cenglin123@users.noreply.github.com>
2024-10-29 20:31:33 +0800

    Update ipfs-cid-copy.user.js

--- ipfs-cid-copy.user.js
+++ ipfs-cid-copy.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         IPFS CID Copy Helper
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.2
 // @description  自动为网页中的 IPFS 链接添加 CID 复制功能，右下角可以显示批量操作窗口。 支持多种 IPFS/IPNS 格式和批量复制。
 // @author       cenglin123
 // @match        *://*/*
@@ -167,7 +167,7 @@
             if (subdomain.match(/^(baf[yk][a-zA-Z0-9]{55}|Qm[a-zA-Z0-9]{44})$/i)) {
                 extractedCID = subdomain;
             }
-            // 更精确的 IPNS key 匹配
+            // 匹配 IPNS key 
             if (subdomain.match(/^k51[a-zA-Z0-9]{59}$/i)) {
                 extractedCID = subdomain;
             }
